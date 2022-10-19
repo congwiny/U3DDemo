b5d6c4bb49bc3823dfb2c57d928dc161745a7df7
niuchong <niuchong@360.cn>
2022-10-19 20:56:51 +0800

    设置全局坐标

---
+++ Assets/Scripts/GameObj/GameObjInfo.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjInfo : MonoBehaviour
+{
+    public GameObject obj;
+    void Start()
+    {
+        Debug.Log("GameObj obj.name="+obj.name);
+
+    
+    }
+
+}
---
+++ Assets/Scripts/Transform/TransformSample.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformSample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //设置的是全局坐标
+        transform.position = new Vector3 (1.5f,1,0);
+    }
+
+}
