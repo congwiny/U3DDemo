128e650216078d9f9212802c3542ef10123151ca
niuchong <niuchong@360.cn>
2022-10-20 10:32:21 +0800

    设置缩放

--- Assets/Scripts/Transform/TransformSample.cs
+++ Assets/Scripts/Transform/TransformSample.cs
@@ -12,7 +12,11 @@ public class TransformSample : MonoBehaviour
         //设置的是局部坐标
         //transform.localPosition = new Vector3(1.5f, 1, 0);
         //设置全局旋转角度
-        transform.eulerAngles = new Vector3(0, 0, 45);
+        //transform.eulerAngles = new Vector3(0, 0, 45);
+        //设置局部旋转角度
+        //transform.localEulerAngles = new Vector3(0, 0, 45);
+        //设置局部缩放
+        transform.localScale = new Vector3(1, 2, 0.1f);
     }
 
 }
