ee63c17e63d822ea260e37b97591f84f114bdd44
niuchong <niuchong@360.cn>
2022-10-20 15:58:39 +0800

    延迟和重复执行

--- Assets/Scripts/Coroutine/Coroutine.cs
+++ Assets/Scripts/Coroutine/Coroutine.cs
@@ -7,16 +7,29 @@ public class Coroutine : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("start ->"+Time.time);
-        StartCoroutine("testCoroutine");
+        Debug.Log("start ->" + Time.time);
+        //StartCoroutine("testCoroutine");
+        //延迟2s调用testInvoke方法
+        //Invoke("testInvoke", 2);
+        InvokeRepeating("testRepeatInvoke", 1, 3);
         Debug.Log("end ->" + Time.time);
     }
 
-    IEnumerator testCoroutine() 
+    IEnumerator testCoroutine()
     {
         Debug.Log("start coroutine ->" + Time.time);
         yield return new WaitForSeconds(1);
         Debug.Log("end coroutine ->" + Time.time);
 
+    }
+
+    void testInvoke()
+    {
+        Debug.Log("test Invoke ->" + Time.time);
+    }
+
+    void testRepeatInvoke()
+    {
+        Debug.Log("test Invoke ->" + Time.time);
     }
 }
