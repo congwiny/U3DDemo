2e274683e151b48939c9981510ba7872176a3e4c
niuchong <niuchong@360.cn>
2022-10-20 11:10:27 +0800

    Tranform 移动位置

---
+++ Assets/Scripts/Move/MoveGameObj.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveGameObj : MonoBehaviour
+{
+    public Transform[] tfs;
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //以自己轴向往某个方向移动
+        tfs[0].Translate(Vector3.right * speed * Time.deltaTime);
+        //以自己轴向往x,y,z轴移动
+        tfs[1].Translate(speed * Time.deltaTime, 0, 0);
+
+        //沿世界坐标轴移动
+        //tfs[2].Translate(Vector3.right * speed * Time.deltaTime, Space.World);
+        tfs[2].Translate(speed * Time.deltaTime, 0, 0, Space.World);
+
+
+    }
+}
